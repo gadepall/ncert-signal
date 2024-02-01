57763be931816d56552a1dc8d1205e56c818d6dc
vs00007 <siddharthavemula07@gmail.com>
2024-02-02 01:57:28 +0530

    Updated 2.1.4

--- ncert-maths/10/5/3/9/codes/plot.py
+++ ncert-maths/10/5/3/9/codes/plot.py
@@ -1,13 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def x(n):
-        return (1+2*n)
 n_values= np.arange(-3, 18, 1)
 n_1=np.array([6,16])
-#y= np.loadtxt("series.txt", delimiter=" ", max_rows=1)
-y=np.heaviside(n_values, 1)*(n_values+1)**2
-y_1=np.heaviside(n_1, 1)*(n_1+1)**2
+y= np.loadtxt("series.txt", delimiter=" ", max_rows=1)
+y_1= [y[i+3] for i in (n_1)]
 
 plt.stem(n_values, y, linefmt='-', markerfmt='o', basefmt='r', label=r'$(n+1)^2$')
 plt.stem(n_1, y_1, 'pink')
