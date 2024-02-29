459d4493ec617d3c415c546f95d04dd50c15b373
p venkata chanakya <155810419+ee23btech11048@users.noreply.github.com>
2024-02-29 18:22:28 +0530

    Add files via upload

---
+++ ncert-maths/10/5/2/3/codes/graph3.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+# Read data from file
+with open('data3.txt', 'r') as file:
+    data = [int(line.strip()) for line in file]
+
+# Plot the data as a stem plot
+plt.stem(range(len(data)), data)
+plt.xlabel('n')
+plt.ylabel('x(n)')
+plt.grid(True)
+plt.savefig('plot3.png')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
