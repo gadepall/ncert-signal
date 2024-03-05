eab5b944f7ea13b8fe686edc382ea003ef487079
Starhauke <73460509+Starhauke@users.noreply.github.com>
2024-03-05 20:18:31 +0530

    Add files via upload

---
+++ ncert-maths/11/9/3/19/codes/s.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    FILE *fp;
+    fp = fopen("yn.dat", "w"); // Open file for writing
+
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    int n;
+    double yn;
+
+    for (n = 0; n <= 4; n++) {
+        yn = (-256.0 / pow(2, n)) + (512.0 / 1);
+        fprintf(fp, "%f\n", yn); // Write y(n) to file
+    }
+
+    fclose(fp); // Close the file
+
+    printf("Data has been saved to yn.dat\n");
+    return 0;
+}
+
+
